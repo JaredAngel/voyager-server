230bdde467b7afc3d3d208d55342edb94900596b
Jared Angel <jared.aescobedo@gmail.com>
2020-12-16 18:48:55 -0600

    bug fixes

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
 
-//app.use(validateBearerToken);
+app.use(validateBearerToken);
 app.use('/api/voyages', voyagesRouter);
 app.use('/api/activities', activitiesRouter);
 app.use('/api/users', usersRouter);
